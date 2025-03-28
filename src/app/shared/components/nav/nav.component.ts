d3ca06cf0ff5268a9044a4230e41bd00f54bdd69
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2025-03-28 13:53:06 -0300

    feat: create route processing and layout adjusts

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { Route, RouterLink, RouterOutlet } from '@angular/router';
+import { PublicRoute } from '../../layout/public/public.routes';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
-  imports: [MatSidenavModule, MatListModule],
+  imports: [MatSidenavModule, MatListModule, RouterOutlet, NgForOf, RouterLink],
   standalone: true,
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
-export class NavComponent {}
+export class NavComponent {
+  navItems: {
+    label: string;
+    route: string;
+    icon: string;
+  }[] = [];
+
+  constructor() {
+    this.getMenuItems(PublicRoute);
+  }
+
+  private getMenuItems(route: Route) {
+    if (route.children) {
+      route.children.forEach((child) => {
+        if (child?.data && child.data['title'] && child.data['icon']) {
+          this.navItems.push({
+            label: child.data['title'],
+            route: '/' + child.path,
+            icon: child.data['icon'],
+          });
+        }
+      });
+    }
+  }
+}
--- src/app/shared/layout/public/public.routes.ts
+++ src/app/shared/layout/public/public.routes.ts
@@ -1,8 +1,21 @@
 import { Route } from '@angular/router';
 import { PublicComponent } from './public.component';
+import { HomeComponent } from '../../../modules/home/home.component';
+import { ProductComponent } from '../../../modules/product/product.component';
 
 export const PublicRoute: Route = {
   path: '',
   component: PublicComponent,
-  children: [],
+  children: [
+    {
+      path: '',
+      component: HomeComponent,
+      data: { title: 'Início', icon: 'home' },
+    },
+    {
+      path: 'product',
+      component: ProductComponent,
+      data: { title: 'Produto', icon: 'storefront' },
+    },
+  ],
 };
