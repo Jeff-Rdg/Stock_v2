7ada52c1727990d5f9305fea9cefafc52e69de7e
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2025-03-29 15:26:19 -0300

    add: create product.service.ts and model

---
+++ src/app/modules/product/services/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ProductModel } from '../model/product.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  baseUrl = 'http://localhost:3000/products';
+  constructor(private http: HttpClient) {}
+
+  create(product: ProductModel): Observable<ProductModel> {
+    return this.http.post<ProductModel>(this.baseUrl, product);
+  }
+}
