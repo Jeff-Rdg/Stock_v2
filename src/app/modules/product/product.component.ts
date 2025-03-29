9789b11777894c3c747710437a44bbefc1681a28
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2025-03-28 22:41:06 -0300

    create: product.component

--- src/app/modules/product/product.component.ts
+++ src/app/modules/product/product.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../shared/components/card/card.component';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CardComponent],
   standalone: true,
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
