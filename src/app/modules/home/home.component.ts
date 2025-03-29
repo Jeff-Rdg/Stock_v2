2b0bff9fc66e7c8b3087eb0f07c26cf317d461e4
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2025-03-28 22:39:03 -0300

    modify: home component

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { CardComponent } from '../../shared/components/card/card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [MatCardModule],
+  imports: [MatCardModule, CardComponent],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
