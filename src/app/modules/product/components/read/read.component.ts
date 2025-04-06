3171b0e235a1d0cc188c3b014b31f0f6adcd7a91
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2025-04-05 23:05:16 -0300

    add: product read component

--- src/app/modules/product/components/read/read.component.ts
+++ src/app/modules/product/components/read/read.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductModel } from '../../model/product.model';
 import { ProductService } from '../../services/product.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-product-read',
-  imports: [],
+  imports: [MatTableModule],
   standalone: true,
   templateUrl: './read.component.html',
   styleUrl: './read.component.scss',
 })
 export class ProductReadComponent implements OnInit {
-  products: ProductModel[] = [];
+  displayedColumns: string[] = ['id', 'name', 'price'];
+  dataSource = new MatTableDataSource<ProductModel>([]);
 
   constructor(private service: ProductService) {}
   ngOnInit(): void {
-    this.service.read().subscribe((products) => {
-      this.products = products;
+    this.service.read().subscribe({
+      next: (products) => {
+        this.dataSource.data = products;
+      },
+      error: (err) => {
+        console.log('aconteceu um erro', err);
+      },
     });
   }
 }
